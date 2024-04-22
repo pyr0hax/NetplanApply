9076eed8245e182ef34738d814154681ff40ba7c
Jaco van Zyl <77932967+pyr0hax@users.noreply.github.com>
2024-04-22 15:28:33 +0200

    Fixed for new versions

--- setipubuntu.sh
+++ setipubuntu.sh
@@ -1,53 +1,44 @@
 #!/bin/bash
-
-list_netplan_configs() {
-    echo "Available netplan configurations:"
-    ls /etc/netplan/*.yaml 2>/dev/null || echo "No netplan configurations found."
-}
-
-select_netplan_config() {
-    list_netplan_configs
-    read -p "Enter the name of the configuration file you want to edit (e.g., 01-netcfg.yaml): " config_file
-    config_path="/etc/netplan/$config_file"
-    if [ -f "$config_path" ]; then
-        echo "Editing $config_path"
-    else
-        echo "Error: Configuration file not found."
-        exit 1
-    fi
-}
-
 read -p "Do you want to use DHCP or Static IP? (dhcp/static): " ip_type
+config=""
+if [[ "$ip_type" == "dhcp" ]]; then
+    config="dhcp4: true"
+elif [[ "$ip_type" == "static" ]]; then
+    read -p "Enter IP address and subnet mask (e.g., 172.26.137.116/20): " ip_subnet
+    read -p "Enter gateway address (e.g., 172.26.128.1): " gateway
+    read -p "Enter DNS servers (comma-separated): " dns_servers
+    config=$(cat <<EOF
+dhcp4: false
+addresses:
+  - $ip_subnet
+routes:
+  - to: 0.0.0.0/0
+    via: $gateway
+nameservers:
+  addresses: [$dns_servers]
+EOF
+    )
+else
+    echo "Invalid option. Please choose either dhcp or static."
+    exit 1
+fi
+echo "Available netplan configurations:"
+ls /etc/netplan/*.yaml 2>/dev/null || echo "No netplan configurations found."
 
-echo "User input: $ip_type"
+read -p "Enter the name of the configuration file you want to edit (e.g., 01-netcfg.yaml): " config_file
+config_path="/etc/netplan/$config_file"
 
-case "$ip_type" in
-    dhcp)
-        config="dhcp4: yes"
-        ;;
-    static)
-        read -p "Enter IP address and subnet mask (e.g., 192.168.1.222/24): " ip_subnet
-        read -p "Enter gateway address: " gateway
-        read -p "Enter DNS servers (comma-separated): " dns_servers
-
-        config="dhcp4: no\n    addresses: [$ip_subnet]\n    gateway4: $gateway\n    nameservers:\n      addresses: [$dns_servers]"
-        ;;
-    *)
-        echo "Invalid option. Please choose either dhcp or static."
-        exit 1
-        ;;
-esac
-
-select_netplan_config
-
+if [ -f "$config_path" ]; then
+    echo "Editing $config_path"
+else
+    echo "Error: Configuration file not found."
+    exit 1
+fi
 cat <<EOF | sudo tee "$config_path"
-# This file describes the network interfaces available on your system
-# For more information, see netplan(5).
 network:
   version: 2
-  renderer: networkd
   ethernets:
-    enp0s3:
+    eth0:
       $config
 EOF
 
